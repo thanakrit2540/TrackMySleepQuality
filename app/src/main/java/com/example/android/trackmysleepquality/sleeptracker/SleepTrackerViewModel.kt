1ae7db243625c7d1c4bedf95928e678b86143f58
tee29909 <59160571@go.buu.ac.th>
2019-10-29 15:55:27 +0700

    six

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -20,6 +20,7 @@ import android.app.Application
 import android.provider.SyncStateContract.Helpers.insert
 import android.provider.SyncStateContract.Helpers.update
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
@@ -45,6 +46,11 @@ class SleepTrackerViewModel(
     init {
         initializeTonight()
     }
+
+    private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+
+    val navigateToSleepQuality: LiveData<SleepNight>
+        get() = _navigateToSleepQuality
 
     private fun initializeTonight() {
         uiScope.launch {
@@ -81,6 +87,7 @@ class SleepTrackerViewModel(
             val oldNight = tonight.value ?: return@launch
             oldNight.endTimeMilli = System.currentTimeMillis()
             update(oldNight)
+            _navigateToSleepQuality.value = oldNight
         }
     }
 
@@ -109,7 +116,14 @@ class SleepTrackerViewModel(
         viewModelJob.cancel()
     }
 
+    fun doneNavigating() {
+        _navigateToSleepQuality.value = null
+    }
+
+
 }
+
+
 
 
 
